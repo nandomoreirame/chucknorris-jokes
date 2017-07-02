5114d443cb0fb25a96ab60ff9dd77c76f72dc909
Fernando Moreira <nandomoreira.me@gmail.com>
2017-07-02 14:44:39 -0300

    Added button for refresh radon joke

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,11 @@
     <div class="joke">
       <p v-show="!loading">👊🏼 {{joke.value}} 👊🏼</p>
       <div v-show="loading" class="loading">{{loadingText}}</div>
+      <button @click="refreshChuckJoke" class="refresh-button" role="button">
+        <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+          <path d="M14.89 2.628C13.358.985 11.168 0 8.76 0 3.94 0 0 3.942 0 8.76c0 4.817 3.942 8.758 8.76 8.758 4.05 0 7.444-2.847 8.43-6.57h-2.3c-.876 2.52-3.284 4.38-6.13 4.38-3.614 0-6.57-2.956-6.57-6.57 0-3.612 2.956-6.568 6.57-6.568 1.86 0 3.393.766 4.598 1.97L9.854 7.665h7.664V0L14.89 2.628z" fill="#A69898"/>
+        </svg>
+      </button>
     </div>
     <a class="share" :href="shareURL" target="_blank">
       <svg width="26" height="35" viewBox="0 0 26 35" xmlns="http://www.w3.org/2000/svg">
@@ -52,17 +57,31 @@ export default {
   },
   mounted() {
     this.loading = true
-    this.$http.get('https://api.chucknorris.io/jokes/random').then(response => {
-      this.joke = response.data
-      this.shareURL = `${this.shareURL}?url=${window.location.href}&text=${this.joke.value}&hashtags=${this.shareHashtags}&via=umdevux`
-      this.loading = false
-    })
+    this.updateJoke()
   },
   methods: {
-    humanizeURL: function (url) {
+    humanizeURL(url) {
       return url
         .replace(/^https?:\/\//, '')
         .replace(/\/$/, '')
+    },
+    updateShareURL(value) {
+      let url = encodeURI(`${window.location.href}`)
+      let text = encodeURI(`${value}`)
+      return `${this.shareURL}?url=${url}&text=${text}&hashtags=${this.shareHashtags}&via=umdevux`
+    },
+    updateJoke () {
+      this.$http.get('https://api.chucknorris.io/jokes/random').then(response => {
+        this.joke = response.data
+        this.shareURL = this.updateShareURL(response.data.value)
+        this.loading = false
+      })
+    },
+    refreshChuckJoke (event) {
+      if (event) {
+        this.loading = true
+        this.updateJoke()
+      }
     }
   }
 }
@@ -121,6 +140,7 @@ a {
   border-radius: 10px;
   margin: 30px auto;
   padding: 30px;
+  position: relative;
 }
 
 .joke p {
@@ -133,6 +153,17 @@ a {
 .share svg {
   display: inline-block;
   vertical-align: bottom;
+}
+
+.refresh-button {
+  border: none;
+  cursor: pointer;
+  background: none transparent;
+  padding: 0;
+  margin: 0;
+  position: absolute;
+  right: 10px;
+  top: 10px;
 }
 
 .loading {
